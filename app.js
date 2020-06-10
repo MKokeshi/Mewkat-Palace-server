46ccd24539de4708531c170c7ec2c4cb089967c5
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-10 21:36:18 +0200

    Add mongodb client

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const mongoConnectionString = process.env.MONGO_CONNECTION;
+
 var app = express();
+
+//Mongo connection
+MongoClient.connect(mongoConnectionString, {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+  })
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
