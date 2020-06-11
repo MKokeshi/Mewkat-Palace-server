4d77e384b434667e33d215c3aa1c5552ee27bc7b
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-11 19:04:49 +0200

    Update docs title

--- docs/api_project.js
+++ docs/api_project.js
@@ -2,13 +2,13 @@ define({
   "name": "Mewkat Palace Server",
   "version": "0.1.0",
   "description": "Mewkat Palace API doc",
-  "title": "Custom apiDoc browser title",
+  "title": "Mewkat Palace Api Documentation",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-06-11T16:59:01.104Z",
+    "time": "2020-06-11T17:04:31.939Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
